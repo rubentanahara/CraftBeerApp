04f491f86ed829caec695e7284d30c625af7b0b1
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-11-20 12:31:05 -0600

    update react-navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppNavigator from './src/navigation/Navigation'; // Adjust the path according to your file structure
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
